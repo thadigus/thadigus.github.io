90b2d355f0b9d36374269ee2f7f12d969c0e1df1
thadigus <turner.w.thad@gmail.com>
2025-07-13 01:49:26 -0400

    Migrated to devcontainers and made script compatible, updated Jekyll to latest version, updated pipeline badge to GitHub

--- Dockerrun.sh
+++ Dockerrun.sh
@@ -4,7 +4,7 @@ if bash -c 'docker images | grep thadigus-glpages'; then
   echo 'Image found locally, proceeding with run...'
 else
   echo 'Image not found, building image...'
-  docker build -t thadigus-glpages ./
+  docker build -t thadigus-glpages:latest -f ./.devcontainer/Dockerfile . 
 fi
 
 docker run --rm -it --network host -v ./:/code thadigus-glpages:latest
